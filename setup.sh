7116c14ad1cb44cb6dc50133eba7cd5ba2a2b06f
Marcus Maggi <marcus@localhost.localdomain>
2020-08-05 00:20:03 -0300

    removing vscode from flatpak and adding rpm resource

--- setup.sh
+++ setup.sh
@@ -24,7 +24,11 @@ dnf install notepadqq -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Visual Studio Code
-flatpak install flathub com.visualstudio.code -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+sudo dnf check-update
+sudo dnf install code
 
 # Terminator
 dnf install terminator -y
